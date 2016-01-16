c7a573c3da311f361d9930608d8e7a42f9506860
Cameron Presley <philipcpresley@gmail.com>
2016-01-15 23:17:45 -0500

    Added create method for issues. Also updated script file.

---
+++ IssueTracker.DataAccess/IssueLayer.fs
@@ -0,0 +1,14 @@
+﻿namespace IssueTracker.DataAccess
+module IssueLayer = 
+    open FSharp.Data
+    open IssueTracker.Core.Models
+
+    [<Literal>]
+    let connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=IssueTracker.Database;Integrated Security=True"
+
+    [<Literal>]
+    let private createQuery = "INSERT INTO Issues (Title, Description, Priority) VALUES (@Title, @Description, @Priority)"
+
+    let createIssue issue =
+        let cmd = new SqlCommandProvider<createQuery, connectionString> ()
+        cmd.Execute(Title=issue.title, Description=issue.description, Priority=(issue.priority |> sprintf("%A")))
---
+++ IssueTracker.DataAccess/Script.fsx
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "..\\IssueTracker.Core\\Models.fs"
+#reference @".\bin\Debug\FSharp.Data.SqlClient.dll"
+#load ".\\IssueLayer.fs"
+open IssueTracker.Core.Models
+open IssueTracker.DataAccess.IssueLayer
+
+// Define your library scripting code here
+
