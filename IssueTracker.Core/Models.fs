c5a9fc9e53c38ae634c4cedeb94e20e7c40d5f49
Cameron Presley <cameron@thesoftwarementor.com>
2016-01-15 22:27:29 -0500

    Added basic models for a user and an issue.

---
+++ IssueTracker.Core/Models.fs
@@ -0,0 +1,7 @@
+﻿namespace IssueTracker.Core
+module Models =
+    type User = {username:string; password:string; initials:string }
+
+    type Priority = ShowStopper | Critical | Annoying | Trivial
+    type Issue = {title:string; description:string; priority:Priority}
+
